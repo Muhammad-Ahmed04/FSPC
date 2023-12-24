7b03ba2fc54f9266b66a56b361b2d354f4591fae
shamil-bxlend <shamil.bxlend@gmail.com>
2023-12-24 22:55:55 +0500

    add pastPaper deletion

--- client/src/components/PastPapers/pastpaper.jsx
+++ client/src/components/PastPapers/pastpaper.jsx
@@ -1,89 +1,138 @@
 import React, { useEffect, useState } from 'react';
-import './pastpaper.css'
+import './pastpaper.css';
 import { useNavigate } from 'react-router-dom';
 import { Header } from '../Header';
+
 const fetchData = async () => {
-    try {
-        const response = await fetch("http://52.200.18.237:8080/api/pastpapers");
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
+  try {
+    const response = await fetch('http://52.200.18.237:8080/api/pastpapers');
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 };
+
 const PastPapers = () => {
-    const [data, setData] = useState([]);
-    const [fetchDataFlag, setFetchDataFlag] = useState(true); // State to trigger data fetching
-    const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [fetchDataFlag, setFetchDataFlag] = useState(true); // State to trigger data fetching
+  const [selectedPaper, setSelectedPaper] = useState(null);
+  const navigate = useNavigate();
 
+  const backToHome = () => {
+    navigate('/home');
+  };
 
-    const backToHome = () => {
-        navigate('/home');
+  const handleDelete = async (paperToDelete) => {
+    try {
+        console.log(`paperToDelete id is ${paperToDelete._id}`);
+        // const id = paperToDelete._id
+      const response = await fetch(`http://52.200.18.237:8080/api/pastpapers`, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+          },
+        body: JSON.stringify({id : paperToDelete._id}),
+      });
+
+      if (response.ok) {
+        // Remove the deleted paper from the data state
+        setData((prevData) => prevData.filter((paper) => paper.id !== paperToDelete.id));
+        navigate(0)
+      } else {
+        console.error('Error deleting paper:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting paper:', error);
+    }
+
+    // Clear the selectedPaper state
+    setSelectedPaper(null);
+  };
+
+  // Call the fetchData function when the component is loaded
+  useEffect(() => {
+    const fetchDataAndSetData = async () => {
+      const result = await fetchData();
+      if (result) {
+        setData(result);
+        setFetchDataFlag(false);
+        console.log(result);
+      }
     };
-    // Call the fetchData function when the component is loaded
-    useEffect(() => {
-        const fetchDataAndSetData = async () => {
-            const result = await fetchData();
-            if (result) {
-                setData(result);
-                setFetchDataFlag(false);
-                console.log(result);
-            }
-        };
-        fetchDataAndSetData();
-    }, [fetchDataFlag]);
+    fetchDataAndSetData();
+  }, [fetchDataFlag]);
 
-    return (
-        <>
+  return (
+    <>
       <Header page="pp"></Header>
 
-        <div style={{ backgroundColor: '#1E252B', minHeight: '100vh', padding: '20px' }}>
-            <button id='back' onClick={backToHome}>
-                Back to Home
-            </button>
+      <div style={{ backgroundColor: '#1E252B', minHeight: '100vh', padding: '20px' }}>
+        <button id="back" onClick={backToHome}>
+          Back to Home
+        </button>
 
-            <div style={{ width:'80%', margin: '0 auto' }}>
-                <table style={{ width: '100%', borderCollapse: 'collapse', color: 'white', lineHeight: '50px' }}>
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Link</th>
-                            <th>Type</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((paper, index) => {
-                            const isValidDate = !!paper.date && !isNaN(Date.parse(paper.date));
+        <div style={{ width: '80%', margin: '0 auto' }}>
+          <table style={{ width: '100%', borderCollapse: 'collapse', color: 'white', lineHeight: '50px' }}>
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Link</th>
+                <th>Type</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((paper, index) => {
+                const isValidDate = !!paper.date && !isNaN(Date.parse(paper.date));
+                const formattedDate = isValidDate
+                  ? (() => {
+                      const dateObject = new Date(paper.date);
+                      return `${(dateObject.getMonth() + 1).toString().padStart(2, '0')}-${dateObject
+                        .getDate()
+                        .toString()
+                        .padStart(2, '0')}-${dateObject.getFullYear()}`;
+                    })()
+                  : '';
 
-                            // If it's a valid date, format it; otherwise, display an empty string
-                            const formattedDate = isValidDate
-                                ? (() => {
-                                    const dateObject = new Date(paper.date);
-                                    return `${(dateObject.getMonth() + 1).toString().padStart(2, '0')}-${dateObject.getDate().toString().padStart(2, '0')}-${dateObject.getFullYear()}`;
-                                })()
-                                : '';
-                            return (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>{paper.name}</td>
-                                    <td>{formattedDate}</td>
-                                    <td>
-                                        <a target='_blank' href={paper.link} style={{ color: 'white', textDecoration: 'none' }} rel="noreferrer">
-                                            {paper.link}
-                                        </a>
-                                    </td>
-                                    <td>{paper.kind}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+                return (
+                  <React.Fragment key={index}>
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{paper.name}</td>
+                      <td>{formattedDate}</td>
+                      <td>
+                        <a target='_blank' href={paper.link} style={{ color: 'white', textDecoration: 'none' }} rel="noreferrer">
+                          {paper.link}
+                        </a>
+                      </td>
+                      <td>{paper.kind}</td>
+                      <td>
+                        <button onClick={() => setSelectedPaper(paper)}>Edit</button>
+                      </td>
+                    </tr>
+
+                    {selectedPaper && selectedPaper.id === paper.id && (
+                      <tr>
+                        <td colSpan="5"> {/* Span across all columns */}
+                          <div className="edit-options">
+                            <button onClick={() => setSelectedPaper(null)}>Cancel</button>
+                            <button onClick={() => handleDelete(selectedPaper)}>Delete</button>
+                          </div>
+                        </td>
+                      </tr>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default PastPapers;
